2c1812bd687d1fe80d7344772f4c89caa491a9dd
Rakesh Kumar <kumar19@ilinois.edu>
2017-11-30 10:50:07 -0600

    More accounting.

--- receive_validate.py
+++ receive_validate.py
@@ -3,33 +3,45 @@ from scapy.all import *
 from coding_packet import CodingPacket
 bind_layers(Ether, CodingPacket, type=0x1234)
 total= 0
-a_pkts = 0
-b_pkts = 0
-x_pkts = 0
+a = 0
+b = 0
+x = 0
 a_max_seqnum = 0
 b_max_seqnum = 0
 x_max_seqnum = 0
+x_with_a = 0 
+x_with_b = 0 
+x_with_x = 0
 
 def print_status(pkt):
-    global total, a_pkts, b_pkts, x_pkts, a_max_seqnum, b_max_seqnum, x_max_seqnum
+    global total, a, b, x, a_max_seqnum, b_max_seqnum, x_max_seqnum
+    global x_with_a, x_with_b, x_with_x
+
     total += 1
     
     if pkt[CodingPacket].packet_contents == 'A':
-        a_pkts += 1
+        a += 1
         if a_max_seqnum < int(pkt[CodingPacket].coded_packets_seqnum):
             a_max_seqnum = int(pkt[CodingPacket].coded_packets_seqnum)
 
     elif pkt[CodingPacket].packet_contents == 'B':
-        b_pkts += 1
+        b += 1
         if b_max_seqnum < int(pkt[CodingPacket].coded_packets_seqnum):
             b_max_seqnum = int(pkt[CodingPacket].coded_packets_seqnum)
 
     elif pkt[CodingPacket].packet_contents == 'X':
-        x_pkts += 1
+        x += 1
         if x_max_seqnum < int(pkt[CodingPacket].coded_packets_seqnum):
             x_max_seqnum = int(pkt[CodingPacket].coded_packets_seqnum)
 
-    return "total: {}, a_pkts: {}, b_pkts: {}, x_pkts: {}, a_max_seqnum: {}, b_max_seqnum: {}, x_max_seqnum: {}".format(total, a_pkts, b_pkts, x_pkts, a_max_seqnum, b_max_seqnum, x_max_seqnum)
+        if pkt[CodingPacket].packet_payload[0] == 'A':
+            x_with_a += 1
+        elif pkt[CodingPacket].packet_payload[0] == 'B':
+            x_with_b += 1
+        elif pkt[CodingPacket].packet_payload[0] == 'X':
+            x_with_x += 1
+
+    return "total: {}, a: {}, b: {}, x: {}, a_max_seqnum: {}, b_max_seqnum: {}, x_max_seqnum: {}, x_with_a: {}, x_with_b: {}, x_with_x: {}".format(total, a, b, x, a_max_seqnum, b_max_seqnum, x_max_seqnum, x_with_a, x_with_b, x_with_x)
 
 def main():
 

5a9aa82fdaeb1c91f441d330c625bac089c8739e
Rakesh Kumar <kumar19@ilinois.edu>
2018-02-27 19:21:28 -0600

    Think I have the times

--- coding_hdr.py
+++ coding_hdr.py
@@ -1,13 +1,33 @@
-from scapy.all import Packet, StrFixedLenField, XByteField, IntField, ShortField
+from scapy.all import Packet, XStrFixedLenField, StrFixedLenField, XByteField, IntField, ShortField
 from scapy.all import FieldLenField, PacketListField
 
 from switch_stats_hdr import SwitchStatsHdr
 
 class CodingHdr(Packet):
     fields_desc = [ 
-                    FieldLenField("num_switch_stats", None, fmt="B", length_of="switch_stats", adjust=lambda pkt,l:l*2+4),
-                    #ShortField("num_switch_stats", 0),
-                    PacketListField("switch_stats", [], SwitchStatsHdr,count_from=lambda pkt:(pkt.num_switch_stats*1)),
+                    #FieldLenField("num_switch_stats", None, fmt="B", length_of="switch_stats", adjust=lambda pkt,l:l*2+4),
+                    #PacketListField("switch_stats", [], SwitchStatsHdr,count_from=lambda pkt:(pkt.num_switch_stats*1)),
+                    XByteField("num_switch_stats", 0x01),
+                    StrFixedLenField("P", "P", length=1),
+                    StrFixedLenField("Four", "4", length=1),
+                    XByteField("version", 0x01),
+                    XByteField("packet_todo", 0x01),
+                    StrFixedLenField("packet_contents", ' ', length=1),
+                    IntField("coded_packets_seqnum", 0),
+                    StrFixedLenField("packet_payload", ' '*100, length=100)]
+
+
+class CodingHdrR(Packet):
+    fields_desc = [ 
+                    #FieldLenField("num_switch_stats", None, fmt="B", length_of="switch_stats", adjust=lambda pkt,l:l*2+4),
+                    #PacketListField("switch_stats", [], SwitchStatsHdr,count_from=lambda pkt:(pkt.num_switch_stats*1)),
+                    XByteField("num_switch_stats", 0x01),
+                    IntField("swid1", 0),
+                    XStrFixedLenField("igt1", "      ", length=6),
+                    IntField("swid2", 0),
+                    XStrFixedLenField("igt2", "      ", length=6),
+                    IntField("swid3", 0),
+                    XStrFixedLenField("igt3", "      ", length=6),
                     StrFixedLenField("P", "P", length=1),
                     StrFixedLenField("Four", "4", length=1),
                     XByteField("version", 0x01),
--- receive_stream.py
+++ receive_stream.py
@@ -1,8 +1,8 @@
 from scapy.all import *
 
-from coding_hdr import CodingHdr
+from coding_hdr import CodingHdrR
 
-bind_layers(Ether, CodingHdr, type=0x1234)
+bind_layers(Ether, CodingHdrR, type=0x1234)
 total= 0
 a = 0
 b = 0
@@ -20,34 +20,34 @@ def print_status(pkt):
 
     total += 1
 
-    print pkt[CodingHdr].num_switch_stats
-    print len(pkt[CodingHdr].switch_stats)
-    print type(pkt[CodingHdr].switch_stats)
-
-    for i in range(pkt[CodingHdr].num_switch_stats):
-        print "Switch ID:,", i, "is:", pkt[CodingHdr].switch_stats[i].swid
-        print "Timestamp:,", i, "is:", pkt[CodingHdr].switch_stats[i].igt
+    print pkt[CodingHdrR].num_switch_stats
+    print pkt[CodingHdrR].swid1
+    print int(pkt[CodingHdrR].igt1.encode('hex'), 16)
+    print pkt[CodingHdrR].swid2
+    print int(pkt[CodingHdrR].igt2.encode('hex'), 16)
+    print pkt[CodingHdrR].swid3
+    print int(pkt[CodingHdrR].igt3.encode('hex'), 16)
     
-    if pkt[CodingHdr].packet_contents == 'A':
+    if pkt[CodingHdrR].packet_contents == 'A':
         a += 1
-        if a_max_seqnum < int(pkt[CodingHdr].coded_packets_seqnum):
-            a_max_seqnum = int(pkt[CodingHdr].coded_packets_seqnum)
+        if a_max_seqnum < int(pkt[CodingHdrR].coded_packets_seqnum):
+            a_max_seqnum = int(pkt[CodingHdrR].coded_packets_seqnum)
 
-    elif pkt[CodingHdr].packet_contents == 'B':
+    elif pkt[CodingHdrR].packet_contents == 'B':
         b += 1
-        if b_max_seqnum < int(pkt[CodingHdr].coded_packets_seqnum):
-            b_max_seqnum = int(pkt[CodingHdr].coded_packets_seqnum)
+        if b_max_seqnum < int(pkt[CodingHdrR].coded_packets_seqnum):
+            b_max_seqnum = int(pkt[CodingHdrR].coded_packets_seqnum)
 
-    elif pkt[CodingHdr].packet_contents == 'X':
+    elif pkt[CodingHdrR].packet_contents == 'X':
         x += 1
-        if x_max_seqnum < int(pkt[CodingHdr].coded_packets_seqnum):
-            x_max_seqnum = int(pkt[CodingHdr].coded_packets_seqnum)
+        if x_max_seqnum < int(pkt[CodingHdrR].coded_packets_seqnum):
+            x_max_seqnum = int(pkt[CodingHdrR].coded_packets_seqnum)
 
-        if pkt[CodingHdr].packet_payload[0] == 'A':
+        if pkt[CodingHdrR].packet_payload[0] == 'A':
             x_with_a += 1
-        elif pkt[CodingHdr].packet_payload[0] == 'B':
+        elif pkt[CodingHdrR].packet_payload[0] == 'B':
             x_with_b += 1
-        elif pkt[CodingHdr].packet_payload[0] == 'X':
+        elif pkt[CodingHdrR].packet_payload[0] == 'X':
             x_with_x += 1
 
     return "total: {}, a: {}, b: {}, x: {}, a_max_seqnum: {}, b_max_seqnum: {}, x_max_seqnum: {}, x_with_a: {}, x_with_b: {}, x_with_x: {}".format(total, a, b, x, a_max_seqnum, b_max_seqnum, x_max_seqnum, x_with_a, x_with_b, x_with_x)

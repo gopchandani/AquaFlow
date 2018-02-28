79a25718b7c982aeb504395b25dbb824939d5919
Rakesh Kumar <kumar19@ilinois.edu>
2018-02-28 05:35:10 -0600

    More missing piece

---
+++ switch_stats_hdr.py
@@ -0,0 +1,6 @@
+from scapy.all import Packet, StrFixedLenField, XByteField, IntField
+
+class SwitchStatsHdr(Packet):
+    fields_desc = [ IntField("swid", 0),
+                  StrFixedLenField("igt", "igtigt", length=6)]
+

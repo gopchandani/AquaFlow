57e55efec82278e417328bfde4ccb6e9cbec6202
Rakesh Kumar <kumar19@ilinois.edu>
2018-03-03 21:32:15 -0600

    Removing p4app.json

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 P4APPRUNNER=./utils/p4apprunner.py
 mkdir -p build
+mv p4app.$1.json p4app.json
 tar -czf build/p4app.tgz * --exclude='build'
 #cd build
 sudo python $P4APPRUNNER p4app.tgz --build-dir ./build
